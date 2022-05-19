6c917e054a517abc2089ba0d79c0a129d7341359
Tolu A <tolu@adesegha.ca>
2022-05-19 05:18:00 -0600

    solve best bridge

--- best-bridge.js
+++ best-bridge.js
@@ -1,9 +1,16 @@
+/*
+ * e: # edges
+ * n: # nodes
+ *
+ * time: O(e)
+ * space: O(n)
+ */
+
 const land = 'L';
 
 const bestBridge = (grid) => {
   const island = findIsland(grid);
-  console.log(island);
-  // return findBestBridge(grid, island);
+  return findBestBridge(grid, island);
 };
 
 const findIsland = grid => {
@@ -12,21 +19,21 @@ const findIsland = grid => {
       if (column === land) return exploreIsland(grid, rowIndex, columnIndex);
 }
 
-const exploreIsland = (grid, rowIndex, columnIndex, visited = new Set()) => {
+const exploreIsland = (grid, rowIndex, columnIndex, visited = new Set(), islands = []) => {
   if (!isInBounds(grid, rowIndex, columnIndex)
     || !isLand(grid, rowIndex, columnIndex)
     || isVisited(visited, rowIndex, columnIndex))
-    return [];
+    return islands;
 
   visited.add(keyFor(rowIndex, columnIndex));
 
-  const island = [[rowIndex, columnIndex]];
-  island.push(...exploreIsland(grid, rowIndex - 1, columnIndex, visited));
-  island.push(...exploreIsland(grid, rowIndex + 1, columnIndex, visited));
-  island.push(...exploreIsland(grid, rowIndex, columnIndex - 1, visited));
-  island.push(...exploreIsland(grid, rowIndex, columnIndex + 1, visited));
+  islands.push([rowIndex, columnIndex]);
+  exploreIsland(grid, rowIndex - 1, columnIndex, visited, islands);
+  exploreIsland(grid, rowIndex + 1, columnIndex, visited, islands);
+  exploreIsland(grid, rowIndex, columnIndex - 1, visited, islands);
+  exploreIsland(grid, rowIndex, columnIndex + 1, visited, islands);
 
-  return island;
+  return islands;
 }
 
 const isInBounds = (grid, rowIndex, columnIndex) =>
@@ -39,13 +46,29 @@ const isVisited = (visited, rowIndex, columnIndex) => visited.has(keyFor(rowInde
 
 const keyFor = (rowIndex, columnIndex) => `${rowIndex},${columnIndex}`;
 
-bestBridge([
-  ["W", "W", "W", "L", "L"],
-  ["L", "L", "W", "W", "L"],
-  ["L", "L", "L", "W", "L"],
-  ["W", "L", "W", "W", "W"],
-  ["W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W"],
-]);
+const findBestBridge = (grid, island) => {
+  const queue = [];
+  for (let node of island) queue.push([node, 0]);
+
+  const visited = new Set();
+
+  while (queue.length) {
+    const [[rowIndex, columnIndex], count] = queue.shift();
+    if (!isInBounds(grid, rowIndex, columnIndex) || isVisited(visited, rowIndex, columnIndex))
+      continue;
+
+    if (isLand(grid, rowIndex, columnIndex) && !inIsland(island, rowIndex, columnIndex))
+      return count - 1;
+
+    visited.add(keyFor(rowIndex, columnIndex));
+    queue.push([[rowIndex + 1, columnIndex], count + 1])
+    queue.push([[rowIndex - 1, columnIndex], count + 1])
+    queue.push([[rowIndex, columnIndex + 1], count + 1])
+    queue.push([[rowIndex, columnIndex - 1], count + 1])
+  }
+}
+
+const inIsland = (island, rowIndex, columnIndex) =>
+  island.some(([row, column]) => row === rowIndex && column === columnIndex);
 
 module.exports = { bestBridge };
